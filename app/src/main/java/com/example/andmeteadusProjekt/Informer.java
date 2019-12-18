99e56d569d85815c6cde88e80bc053d525c06323
Anneli Klamas <annelik98@gmail.com>
2019-12-18 14:02:11 +0200

    Changed applicatioId

--- app/src/main/java/com/example/andmeteadusProjekt/Informer.java
+++ app/src/main/java/com/example/andmeteadusProjekt/Informer.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.andmeteadusProjekt;
 
 public interface Informer {
     void onTaskDone(String output);
--- app/src/main/java/com/example/andmeteadusProjekt/MainActivity.java
+++ app/src/main/java/com/example/andmeteadusProjekt/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.andmeteadusProjekt;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
